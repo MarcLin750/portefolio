8c8771d8a2fb9611f5efd70b575454501888c94a
Lin Marc <marclin750@gmail.com>
2024-02-23 23:02:48 +0100

    add navbar

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -3,7 +3,22 @@ import React, { FunctionComponent } from "react";
 const NavBar: FunctionComponent = () => {
     return (
         <div>
-            <h1>Nav Bar</h1>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container-fluid">
+                <a className="navbar-brand" href="/portefolio">Marc Lin</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div className="navbar-nav">
+                    <a className="nav-link active" aria-current="page" href="#">Projets</a>
+                    <a className="nav-link" href="#">CV</a>
+                    <a className="nav-link" href="#">Veilles</a>
+                    <a className="nav-link" href="#"> Contact</a>
+                  </div>
+                </div>
+              </div>
+            </nav>
         </div>
     )
 }
